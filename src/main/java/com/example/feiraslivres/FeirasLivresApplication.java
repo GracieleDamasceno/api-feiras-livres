57518064530ebf9d00995309d168ddddd1870870
Graciele Damasceno <56450271+GracieleDamasceno@users.noreply.github.com>
2021-09-29 15:06:51 -0300

    fix: adjustment in project structure

---
+++ src/main/java/com/example/feiraslivres/FeirasLivresApplication.java
@@ -0,0 +1,13 @@
+package com.example.feiraslivres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeirasLivresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeirasLivresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/feiraslivres/FeirasLivresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feiraslivres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeirasLivresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
