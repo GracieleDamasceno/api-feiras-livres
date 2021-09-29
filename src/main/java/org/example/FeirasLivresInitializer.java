5a237392d0616dae6c9e390be697265b2487ec8f
Graciele Damasceno <56450271+GracieleDamasceno@users.noreply.github.com>
2021-09-29 10:38:45 -0300

    feat: setting project structure

---
+++ src/main/java/org/example/FeirasLivresInitializer.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeirasLivresInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(FeirasLivresInitializer.class, args);
+    }
+}
